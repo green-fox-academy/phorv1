106f162e7c2ce2aa183a41509a0de907fc6f2254
Patrik <horvath.patrik001@gmail.com>
2017-03-21 11:43:10 +0100

    Hello me & 3 mates, peom reorder

---
+++ week2/Day2/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Reka!");
+        System.out.println("Hello, Gabor!");
+        System.out.println("Hello, Milan!");
+    }
+}
