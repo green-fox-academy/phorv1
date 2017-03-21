219b596e13b9da98f87bd566929b2e66bc3a3a06
Patrik <horvath.patrik001@gmail.com>
2017-03-21 20:20:48 +0100

    Exercise 28 - add * piramid

--- week2/Day2/src/Ex29.java
+++ week2/Day2/src/Ex29.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ex28 {
+public class Ex29 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please type 1 number!");
@@ -14,6 +14,16 @@ public class Ex28 {
             for (int j = 0; j <= i * 2; j++) {
                 System.out.print("*");
             }
+            System.out.println();
+        }
+        for (int i = 0; i < number; i++) {
+            for (int k = i; k < number*2; k++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j <= i; j++) {
+            }
+            System.out.print(" ");
+
             System.out.println();
         }
     }
