bee910082142e2e8dbcc3bdd4fe1cd1db904d115
Patrik <horvath.patrik001@gmail.com>
2017-03-21 12:19:35 +0100

    Intro me, Playing with 2 numbers and create a complex calculation

---
+++ week2/Day2/src/CodingHours.java
@@ -0,0 +1,14 @@
+public class CodingHours {
+    public static void main(String[] args) {
+        int studentDaily = 6;
+        int semesterLenghts = 17;
+        int workDays = 5;
+        int avgWorkHours = 52;
+        float totalCoded = (studentDaily * semesterLenghts * workDays);
+        float totalHours = (avgWorkHours * semesterLenghts);
+        float percentage = (totalCoded / totalHours) * 100;
+        System.out.println("Hours spent with coding by the attendee: " + totalCoded);
+
+        System.out.println("Percentage of coded hours: " + percentage + "%");
+    }
+}
---
+++ week2/Day2/src/IntroduceMyself.java
@@ -0,0 +1,8 @@
+public class IntroduceMyself {
+    public static void main(String[] args) {
+        System.out.println("Patrik Horvath");
+        System.out.println("24");
+        System.out.println("1.85");
+
+    }
+}
