ef55e689955de5d489f0326988f07bf5a2bce873
Patrik <horvath.patrik001@gmail.com>
2017-03-23 12:26:21 +0100

    Create a list in new lines with \n and tabs with \t

---
+++ week2/day4/src/TodoPrint.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class TodoPrint {
+    public static void main(String... args){
+       String todoText = "My todo:\n" + " - Buy milk\n" + " - Download games\n" + "\t- Diablo";
+         System.out.println(todoText);
+    }
+}
