3fffa988a2db43a2ca2f030e4605720f1b859f05
Patrik <horvath.patrik001@gmail.com>
2017-03-23 13:03:22 +0100

    Add 'Saturn' to 5th position of ArrayList planet

---
+++ week2/day4/src/SolarSystem.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class SolarSystem{
+    public static void main(String... args){
+        ArrayList<String> planetList = new ArrayList<>(Arrays.asList("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune"));
+        planetList.add(5, "Saturn");
+        System.out.println(planetList);
+    }
+}
