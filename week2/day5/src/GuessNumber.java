c58632303bb107da0a1c4a5d18a241d3587b9f28
Patrik <horvath.patrik001@gmail.com>
2017-03-24 11:59:43 +0100

    Guess a random number made simplier

--- week2/day5/src/GuessNumber.java
+++ week2/day5/src/GuessNumber.java
@@ -1,40 +1,42 @@
 import java.util.Scanner;
 
 public class GuessNumber {
-
     public static void main(String[] args) {
+        int smallNumber, bigNumber, lives, autRandomNum, guess;
+        System.out.print("Guess my chosen number!\n");
+        System.out.print("Enter a between range:");
         Scanner scanner = new Scanner(System.in);
-        boolean gotANumber = false;
-        while (!gotANumber) {
-            System.out.println("Please, think of a random number between 0 and 200! Type: \"Got it!\" if you're ready.");
-            String userInput = scanner.nextLine();
-            if ("Got it!".equals(userInput)) {
-                System.out.println("Cool, let's play!");
-                gotANumber = true;
-            }
-        }
+        smallNumber = scanner.nextInt();
+        bigNumber = scanner.nextInt();
+        System.out.print("Your lives: ");
+        lives = scanner.nextInt();
+        autRandomNum = smallNumber + (int) (Math.random() * bigNumber);
+        System.out.println("Random number: (" + autRandomNum + ")");
 
-        int userInput = scanner.nextInt();
-        int a = 0;
-        int b = 100;
-        int c = userInput;
-        boolean d = false;
-        while (!d) {
-            System.out.println("Is this your number: " + c + "? (Say yes or no)");
-            if ("yes".equals(userInput)) {
-                d = true;
-            } else {
-                System.out.println("Is bigger? (Say yes or no)");
-                String isItBigger = scanner.nextLine();
-                if ("yes".equals(isItBigger)) {
-                    a = c;
-                    c += (b - a) / 2;
-                } else {
-                    b = c;
-                    c -= (b - a) / 2;
-                }
+        while (lives > 0) {
+            String range = "(" + smallNumber + ")-(" + bigNumber + ")";
+            System.out.println("Guess now:" + "[between:(" + range + "]");
+            guess = scanner.nextInt();
+            String notInRange = "Your guess:(" + guess + ") isn't within the range " + range;
+            if (guess == autRandomNum) {
+                System.out.println("You Won!");
+                break;
+            } else if (guess > autRandomNum) {
+                lives -= 1;
+                String livesLeft = lives + " lives left)";
+                System.out.println("Number is smaller (" + livesLeft);
+                if (guess > bigNumber){
+                    System.out.println(notInRange);
+                } else {bigNumber = guess;}
+            } else if (guess < autRandomNum) {
+                lives -= 1;
+                String livesLeft = lives + " lives left)";
+                System.out.println("Number is bigger (" + livesLeft);
+                if (guess < smallNumber){
+                    System.out.println(notInRange);
+                } else {smallNumber = guess;}
             }
         }
-        System.out.println("Cool, " + c + " was a pretty good number to guess, awesome.");
+        System.out.println("You Lost!");
     }
 }
