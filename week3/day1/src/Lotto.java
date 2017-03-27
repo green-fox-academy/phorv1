e688324dd68f3139858386c5465d2ba5eec38a43
Patrik <horvath.patrik001@gmail.com>
2017-03-27 12:42:33 +0200

    Count appearance of each numbers

--- week3/day1/src/Lotto.java
+++ week3/day1/src/Lotto.java
@@ -2,15 +2,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Lotto {
 
     public static void main(String[] args) {
         Path numbers = Paths.get("assets/otos.csv");
-            int pieces = 0;
 
         try {
             List<String> text = Files.readAllLines(numbers);
@@ -20,13 +21,15 @@ public class Lotto {
                 String[] column = line.split(";");
 
                 for (int i = column.length-5; i < column.length; i++) {
-                keyMap.put(column [i], pieces);
-                int numbersAppers = pieces ++;
                     if (keyMap.containsKey(column[i])) {
-                        keyMap.put(column[i], numbersAppers);
+                        keyMap.put(column[i], keyMap.get(column[i]) + 1);
+                    } else {
+                        keyMap.put(column[i], 0);
                     }
-                    System.out.println(column[i] + "|" + numbersAppers);
                 }
+            }
+            for (Entry x : keyMap.entrySet()) {
+                System.out.println(x.getKey() + "|" + x.getValue());
             }
         } catch (IOException e) {
             e.printStackTrace();
