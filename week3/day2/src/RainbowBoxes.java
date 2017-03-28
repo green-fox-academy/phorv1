bec24277111d2bc08f7c160341e48cb058f3beb5
Patrik <horvath.patrik001@gmail.com>
2017-03-28 12:59:17 +0200

    Draw Rainbow boxes in the middle of the canvas

--- week3/day2/src/RainbowBoxes.java
+++ week3/day2/src/RainbowBoxes.java
@@ -1,20 +1,28 @@
 import javax.swing.*;
-
 import java.awt.*;
+import java.util.Random;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class CenteredBoxes {
+public class RainbowBoxes {
 
     public static void mainDraw(Graphics graphics){
-        squareDrawingFunction(10, graphics);
-        squareDrawingFunction(20, graphics);
-        squareDrawingFunction(30, graphics);
-
+        sizeColor(20,200, graphics);
     }
-    public static void squareDrawingFunction(int size, Graphics graphics) {
-        graphics.setColor(Color.BLUE);
-        graphics.drawRect(150-size/2,150-size/2,size,size);
+
+    public static void sizeColor(int size, int color, Graphics graphics) {
+        int xx = 300;
+        int random = color;
+
+        while (xx > size){
+            graphics.setColor(new Color(random));
+            graphics.fillRect(150 - xx / 2, 150 - xx/ 2, xx, xx);
+            xx -= 10;
+            random = (int) (Math.random() * 16777215);
+        }
+
+        graphics.setColor (new Color(color));
+        graphics.fillRect(150 - size / 2, 150 - size / 2, size, size);
     }
 
     public static void main(String[] args) {
@@ -33,5 +41,4 @@ public class CenteredBoxes {
 
         }
     }
-
 }
