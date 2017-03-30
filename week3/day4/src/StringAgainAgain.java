4c8bbf03bb6c239e714ce7d267f19907676ac5c3
Patrik <horvath.patrik001@gmail.com>
2017-03-30 15:19:49 +0200

    Add Fibonacci - Calculates the value of the nth element of the fibo

--- week3/day4/src/StringAgainAgain.java
+++ week3/day4/src/StringAgainAgain.java
@@ -1,17 +1,17 @@
 public class StringAgainAgain {
   public static void main(String[] args) {
     String words = "xxxxxxxxxXXXXalfm lakmf";
-    char from = ' ';
+    String from = " ";
     String to = "*";
     System.out.println(replace(words, from, to));
   }
 
-  public static String replace(String s, char from, String to){
+  public static String replace(String s, String from, String to){
     if (s.length() < 1) {
       return s + to;
     } else {
       String letter;
-      if (s.charAt(0) != from) {
+      if (s.valueOf(0) != from) {
         letter = s.charAt(0) + to;
       } else{
         letter = "";
