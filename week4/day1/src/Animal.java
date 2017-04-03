3694cd2c796e29a9416835c91804ab19ce6fa9d4
Patrik <horvath.patrik001@gmail.com>
2017-04-03 15:44:21 +0200

    Animals - Hunger and thirst = 50; eat & drink decreases value and play increases

---
+++ week4/day1/src/Animal.java
@@ -0,0 +1,19 @@
+
+public class Animal {
+    int hunger;
+    int thirst;
+  public Animal() {
+    hunger = 50;
+    thirst = 50;
+  }
+  public void eat(){
+    hunger--;
+  }
+  public void drink(){
+    thirst--;
+  }
+  public void play(){
+    hunger++;
+    thirst++;
+  }
+}
