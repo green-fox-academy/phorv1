a527005a88958af786cce2a53224b20a2071dba6
Patrik <horvath.patrik001@gmail.com>
2017-04-05 12:20:04 +0200

    SumApp - Create multiple test methodes

--- week4/day3/src/SumApp/SumTest.java
+++ week4/day3/src/SumApp/SumTest.java
@@ -1,5 +1,6 @@
 package SumApp;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,12 +10,38 @@ import static org.junit.Assert.*;
 
 public class SumTest {
 
+  List<Integer> list = new ArrayList<>();
+  Sum listOfIntegers = new Sum();
+
   @Test
-  public void getSumAll() throws Exception {
-  ArrayList<Integer> list = new ArrayList<>();
-  list.add(2);
-  list.add(4);
-    Sum listOfIntegers = new Sum();
-    assertEquals(6,listOfIntegers.getSumAll(list));
+  public void sumAll1() throws Exception {
+    list.add(2);
+    list.add(4);
+    assertEquals(6, listOfIntegers.getSumAll(list));
+  }
+
+  @Test
+  public void emptyList() throws Exception {
+    assertEquals(0, listOfIntegers.getSumAll(list));
+  }
+
+  @Test
+  public void listWithOne() throws Exception {
+    list.add(34);
+    assertEquals(34, listOfIntegers.getSumAll(list));
+  }
+
+  @Test
+  public void listWithMultiple() throws Exception {
+    list.add(34);
+    list.add(56);
+    list.add(2);
+    assertEquals(92, listOfIntegers.getSumAll(list));
+  }
+
+  @Test
+  public void withNull() throws Exception {
+    list = null;
+    assertEquals(null, list);
   }
 }
