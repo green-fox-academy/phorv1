857a6145a2e1e0d6bc04517c527dfec80f2ee1fd
Patrik <horvath.patrik001@gmail.com>
2017-04-18 16:13:16 +0200

    Instrument - Relocate variables

--- week6/day1/src/main/java/music/Instrument.java
+++ week6/day1/src/main/java/music/Instrument.java
@@ -2,6 +2,8 @@ package main.java.music;
 
 public abstract class Instrument {
 
+  int numberOfStrings;
+  String sound;
   String name;
 
   abstract void play();
--- week6/day1/src/main/java/music/StringedInstrument.java
+++ week6/day1/src/main/java/music/StringedInstrument.java
@@ -2,9 +2,6 @@ package main.java.music;
 
 public abstract class StringedInstrument extends Instrument{
 
-  int numberOfStrings;
-  String sound;
-  String name;
   final String formatForPlay ="%s, a %d-stringed instrument that %s\n";
 
   @Override
