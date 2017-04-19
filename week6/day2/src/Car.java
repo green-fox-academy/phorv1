48934287189e8fc5614e9e6dd43daf71108e76b1
Patrik <horvath.patrik001@gmail.com>
2017-04-19 11:05:49 +0200

    Car enum - Initial commit

---
+++ week6/day2/src/Car.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+
+  public enum carType{ BMW, AUDI, MERCEDES, VOLKSWAGEN}
+  public enum carColor{RED, BLACK, SILVER, WHITE}
+
+  List<carType> listOFCars = new ArrayList<>();
+  
+}
+
+
+  /*Create a Car class with 2 enum properties
+        enum of car types
+        The types of these cars or vehicles is up to you
+        enum of colors
+    Create 256 Vehicles randomly and put them into a List
+    Count and Print the number of same vehicles for each type
+    Count and Print the number of same vehicles for each color
+    Print the most frequently occurring vehicle*/
