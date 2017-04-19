371c1fd32ebdf28cb85da785074462ae04346efa
Patrik <horvath.patrik001@gmail.com>
2017-04-19 13:47:22 +0200

    Print random number of types and colors

--- week6/day2/src/App.java
+++ week6/day2/src/App.java
@@ -10,7 +10,9 @@ public class App {
       Car car = new Car(randomType(), randomColor());
       parkingLot.add(car);
     }
-    System.out.println(parkingLot);
+    sameType(parkingLot);
+    sameColor(parkingLot);
+
   }
 
   public static carType randomType() {
@@ -45,6 +47,48 @@ public class App {
     return color;
   }
 
+  public static void sameType(List<Car> list) {
+    int countBMW = 0;
+    int countAudi = 0;
+    int countMercedes = 0;
+    int countVolkswagen = 0;
+
+    for (Car car : list) {
+      if (car.type == carType.BMW) {
+        countBMW++;
+      } else if (car.type == carType.AUDI) {
+        countAudi++;
+      } else if (car.type == carType.MERCEDES) {
+        countMercedes++;
+      } else {
+        countVolkswagen++;
+      }
+    }
+    System.out.println("BMW: " + countBMW + " Audi: " + countAudi + " Mercedes: " + countMercedes
+            + " Volkswagen: " + countVolkswagen);
+  }
+
+  public static void sameColor(List<Car> list) {
+    int countRed = 0;
+    int countBlack = 0;
+    int countSilver = 0;
+    int countWhite = 0;
+
+    for (Car car : list) {
+      if (car.color == carColor.RED) {
+        countRed++;
+      } else if (car.color == carColor.BLACK) {
+        countBlack++;
+      } else if (car.color == carColor.SILVER) {
+        countSilver++;
+      } else {
+        countWhite++;
+      }
+    }
+    System.out.println(
+            "Red: " + countRed + " Black: " + countBlack + " Silver: " + countSilver + " White: "
+                    + countWhite);
+  }
 }
 
 
