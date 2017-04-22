fd5a70acd4af65f71c4824272a09727021ad3834
Patrik <horvath.patrik001@gmail.com>
2017-04-22 17:21:17 +0200

    CreditCardApp - saved the cards in a list and print the list out

--- week6/day1/src/creditCard/CreditCardApp.java
+++ week6/day1/src/creditCard/CreditCardApp.java
@@ -1,12 +1,19 @@
 package creditCard;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreditCardApp {
 
   public static void main(String[] args) {
+    List<CreditCard> creditCards = new ArrayList<>();
     for (int i = 0; i < 10; i++) {
     CreditCard creditCard = new CreditCard();
-      System.out.println(creditCard.toString());
+    creditCards.add(creditCard);
+    }
+    for (CreditCard card : creditCards) {
+      System.out.println(card);
     }
   }
 }
