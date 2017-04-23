b93c149c62c5dbf7d1358ba737c61836e429e1fa
Patrik <horvath.patrik001@gmail.com>
2017-04-23 10:37:20 +0200

    CreditCardRes - move implements from app to CCR class

--- week6/day1/src/CreditCardReservation/CreditCardReservation.java
+++ week6/day1/src/CreditCardReservation/CreditCardReservation.java
@@ -1,5 +1,37 @@
 package CreditCardReservation;
 
-public class CreditCardReservation {
+import Reservation.Reservationy;
+import creditCard.CreditCardy;
+
+public class CreditCardReservation implements Reservationy, CreditCardy {
+  @Override
+  public String getDowBooking() {
+    return null;
+  }
+
+  @Override
+  public String getCodeBooking() {
+    return null;
+  }
+
+  @Override
+  public int getSumCVV() {
+    return 0;
+  }
+
+  @Override
+  public String getNameCardholder() {
+    return null;
+  }
+
+  @Override
+  public String getCodeAccount() {
+    return null;
+  }
+
+  @Override
+  public int cumeSumCVV(long codeAccount) {
+    return 0;
+  }
 
 }
--- week6/day1/src/CreditCardReservation/CreditCardReservationApp.java
+++ week6/day1/src/CreditCardReservation/CreditCardReservationApp.java
@@ -1,37 +1,6 @@
 package CreditCardReservation;
 
-import Reservation.Reservationy;
-import creditCard.CreditCardy;
+public class CreditCardReservationApp {
 
-public class CreditCardReservationApp implements Reservationy, CreditCardy {
 
-  @Override
-  public String getDowBooking() {
-    return null;
-  }
-
-  @Override
-  public String getCodeBooking() {
-    return null;
-  }
-
-  @Override
-  public int getSumCVV() {
-    return 0;
-  }
-
-  @Override
-  public String getNameCardholder() {
-    return null;
-  }
-
-  @Override
-  public String getCodeAccount() {
-    return null;
-  }
-
-  @Override
-  public int cumeSumCVV(long codeAccount) {
-    return 0;
-  }
 }
