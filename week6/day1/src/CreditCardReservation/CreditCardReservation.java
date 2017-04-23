28dedd30ffae6f40ef4886f12b65ce68c975fdbc
Patrik <horvath.patrik001@gmail.com>
2017-04-23 11:09:43 +0200

    CreditCardRes - get the expected outcome

--- week6/day1/src/CreditCardReservation/CreditCardReservation.java
+++ week6/day1/src/CreditCardReservation/CreditCardReservation.java
@@ -7,45 +7,61 @@ import java.util.List;
 
 public class CreditCardReservation implements Reservationy, CreditCardy {
 
+  private int sum, ordinalNumber;
+  long codeAccount;
+  private String name;
+  private static int counter = 0;
+  static final String CARD_RES_FORMAT = TEXT + " paid by " + FORMAT;
+
+  public CreditCardReservation() {
+    this.ordinalNumber = counter++;
+    codeAccount = randomGeneratorCC(16);
+    this.name = "DEF" + ordinalNumber ;
+    this.sum = cumeSumCVV(codeAccount);
+  }
 
 
   @Override
   public String getDowBooking() {
-
-    return null;
+    return randomGeneratorBooking(1, DOW);
   }
 
   @Override
   public String getCodeBooking() {
-    return null;
+    return randomGeneratorBooking(8, NULL_TO_Z);
   }
 
   @Override
   public int getSumCVV() {
-    return 0;
+    return sum;
   }
 
   @Override
   public String getNameCardholder() {
-    return null;
+    return name;
   }
 
   @Override
   public String getCodeAccount() {
-    return null;
+    return String.valueOf(codeAccount);
   }
 
   @Override
   public int cumeSumCVV(long codeAccount) {
-    return 0;
+    String[] codeAccountElements = String.valueOf(codeAccount).split("");
+    int sum = 0;
+    for (int i = 0; i < codeAccountElements.length; i++) {
+      sum += Integer.parseInt(codeAccountElements[i]);
+    }
+    return sum;
   }
 
   @Override
   public String toString() {
-    return super.toString();
+    return String.format(CARD_RES_FORMAT, getCodeBooking(), getDowBooking(), getNameCardholder(), getCodeAccount(), getSumCVV());
   }
 
-  String randomGenerator(int amountToRandomise, List<String> getRandomFrom) {
+  String randomGeneratorBooking(int amountToRandomise, List<String> getRandomFrom) {
     String randomCode = "";
     for (int i = 0; i < amountToRandomise; i++) {
       int randomIndex = (int) (Math.random() * getRandomFrom.size());
@@ -60,5 +76,20 @@ public class CreditCardReservation implements Reservationy, CreditCardy {
       System.out.println(reservation);
     }
   }
+
+  long randomGeneratorCC(int amountToRandomise) {
+    String randomCode = "";
+    for (int i = 0; i < amountToRandomise; i++) {
+      if (i == 0) {
+        int randomNumber = 1 + (int) (Math.random() * 9);
+        randomCode += String.valueOf(randomNumber);
+      } else {
+        int randomNumber = (int) (Math.random() * 10);
+        randomCode += String.valueOf(randomNumber);
+      }
+    }
+    return Long.parseLong(randomCode);
+  }
+
 
 }
--- week6/day1/src/CreditCardReservation/CreditCardReservationApp.java
+++ week6/day1/src/CreditCardReservation/CreditCardReservationApp.java
@@ -1,8 +1,20 @@
 package CreditCardReservation;
 
+import creditCard.CreditCard;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CreditCardReservationApp {
 
   public static void main(String[] args) {
+    List<CreditCardReservation> creditCardResList = new ArrayList<>();
+    for (int i = 0; i < 10; i++) {
+      CreditCardReservation creditCardRes = new CreditCardReservation();
+      creditCardResList.add(creditCardRes);
+    }
+    for (CreditCardReservation card : creditCardResList) {
+      System.out.println(card);
+    }
 
   }
 
