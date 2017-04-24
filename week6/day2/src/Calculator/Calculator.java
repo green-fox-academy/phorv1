d220b8835667728cc687832833da166142cacbf5
Patrik <horvath.patrik001@gmail.com>
2017-04-24 15:09:30 +0200

    Calculator - initial commit

---
+++ week6/day2/src/Calculator/Calculator.java
@@ -0,0 +1,22 @@
+package Calculator;
+
+public class Calculator {
+
+  public static void main(String[] args) {
+
+  }
+
+  static <T extends Number> void add(){
+  }
+
+  static <T extends Number> void substract() {
+  }
+
+  static <T extends Number> void miltiply(){
+  }
+
+  static <T extends Number>  void divide() {
+  }
+
+}
+
