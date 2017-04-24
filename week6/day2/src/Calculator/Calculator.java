76df1eb0c76a7b722f3e9704a95bbeb548613502
Patrik <horvath.patrik001@gmail.com>
2017-04-24 15:36:52 +0200

    Calculator - create add and subtract generic methods

--- week6/day2/src/Calculator/Calculator.java
+++ week6/day2/src/Calculator/Calculator.java
@@ -1,17 +1,32 @@
 package Calculator;
 
+import TwentyPlusOne.Game;
+import sun.net.www.content.text.Generic;
+
 public class Calculator {
 
-  static <T extends Number> void add(){
+  static <T extends Number> T add(T... numbers) {
+    Double sum = Double.valueOf(0);
+    for (int i = 0; i < numbers.length; i++) {
+      T elements = numbers[i];
+      sum += elements.doubleValue();
+    }
+    return (T) sum;
   }
 
-  static <T extends Number> void substract() {
+  static <T extends Number> T subtract(T... numbers) {
+    Double sum = Double.valueOf(0);
+    for (int i = 0; i < numbers.length; i++) {
+      T elemts = numbers[i];
+      sum -= elemts.doubleValue();
+    }
+    return (T) sum;
   }
 
-  static <T extends Number> void miltiply(){
+  static <T extends Number> void miltiply() {
   }
 
-  static <T extends Number>  void divide() {
+  static <T extends Number> void divide() {
   }
 
 }
--- week6/day2/src/Calculator/CalculatorApp.java
+++ week6/day2/src/Calculator/CalculatorApp.java
@@ -4,7 +4,9 @@ package Calculator;
 public class CalculatorApp {
 
   public static void main(String[] args) {
-    
-  }
+    System.out.println(Calculator.add(1, 2, 4, 3.1, 34.2222, -34));
+    System.out.println(Calculator.subtract(1, 2, 4, 3.1, 34.2222, -34));
 
+
+  }
 }
