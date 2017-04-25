44557faedf56c283871233b2e865f1ea142e427b
Patrik <horvath.patrik001@gmail.com>
2017-04-25 18:20:43 +0200

    WheatherChecker - correct API URL

--- week6/day3/weatherchecker/src/main/java/Main.java
+++ week6/day3/weatherchecker/src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
   public static void main(String[] args) {
     Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://mxrck-ser-programadores-apis.p.mashape.com/")
+            .baseUrl("https://simple-weather.p.mashape.com/")
             .build();
 
     WeatherChecker service = retrofit.create(WeatherChecker.class);
