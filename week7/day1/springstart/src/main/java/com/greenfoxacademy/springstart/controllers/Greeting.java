57e7d21b024d3410476eb2c54d357431973fd7af
Patrik <horvath.patrik001@gmail.com>
2017-05-02 11:13:07 +0200

    springstart - add greeting class

---
+++ week7/day1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Greeting {
+
+  long id;
+  String content;
+}
