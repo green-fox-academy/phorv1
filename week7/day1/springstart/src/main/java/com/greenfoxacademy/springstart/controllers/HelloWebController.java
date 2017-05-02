4c904b0f8a6f8ec670b3db5dee77948d03971a3f
Patrik <horvath.patrik001@gmail.com>
2017-05-02 11:57:45 +0200

    springstart - add HelloWebController

---
+++ week7/day1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -0,0 +1,21 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWebController {
+
+  private final AtomicLong counter = new AtomicLong();
+
+  @RequestMapping("/web/greeting")
+  public String greeting(@RequestParam(value = "name", defaultValue = "World") String name,
+          Model model) {
+    model.addAttribute("name", name);
+    model.addAttribute("counter", counter.incrementAndGet());
+    return "greeting";
+  }
+}
