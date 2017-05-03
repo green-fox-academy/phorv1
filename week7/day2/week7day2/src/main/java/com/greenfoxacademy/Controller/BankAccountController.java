dd6921b1e605af7152be9e9f65342db44a57cf7c
Patrik <horvath.patrik001@gmail.com>
2017-05-03 13:58:25 +0200

    BankAccount - print all elements of the array list

--- week7/day2/week7day2/src/main/java/com/greenfoxacademy/Controller/BankAccountController.java
+++ week7/day2/week7day2/src/main/java/com/greenfoxacademy/Controller/BankAccountController.java
@@ -1,13 +1,14 @@
 package com.greenfoxacademy.Controller;
 
 import com.greenfoxacademy.Model.BankAccount;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BankAccountController {
-
 
   @RequestMapping(value = "/Exercise1")
   public String bankAccount(Model model) {
@@ -21,5 +22,17 @@ public class BankAccountController {
     String inputString = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
     model.addAttribute("inputString", inputString);
     return "this-is-html";
+  }
+
+  @RequestMapping(value = "/Exercise5")
+  public String showArrayList(Model model) {
+    List<BankAccount> bankAccounts = new ArrayList<>();
+    bankAccounts.add(new BankAccount("Pumba", 101, "warthog"));
+    bankAccounts.add(new BankAccount("Timon", 100, "meerkat "));
+    bankAccounts.add(new BankAccount("Rafiki", 4000, "warthog"));
+    bankAccounts.add(new BankAccount("Mufasa", 6789, "lion"));
+    bankAccounts.add(new BankAccount("Zazu", 10, "hornbill"));
+    model.addAttribute("bankAccounts", bankAccounts);
+    return "arraylist";
   }
 }
