8b08c1ce3e0a8331cd4d6a7627646aa53ae57f92
Patrik <horvath.patrik001@gmail.com>
2017-05-03 11:36:11 +0200

    BankAccount - add bank account controller

---
+++ week7/day2/week7day2/src/main/java/com/greenfoxacademy/Controller/BankAccountController.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy.Controller;
+
+import com.greenfoxacademy.Model.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BankAccountController {
+
+
+  @RequestMapping(value = "/Exercise1")
+  public String bankAccount(Model model) {
+    BankAccount bankAccount = new BankAccount("Simba", 2000, "lion");
+    model.addAttribute(bankAccount);
+    return "bank-account";
+  }
+}
---
+++ week7/day2/week7day2/src/main/java/com/greenfoxacademy/Model/BankAccount.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BankAccount {
+
+  String name;
+  float balance;
+  String animalType;
+}
