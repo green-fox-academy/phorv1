68db8823aa5ac6ea2c67d7ffafdcf952167bd7c7
Patrik <horvath.patrik001@gmail.com>
2017-05-04 14:46:41 +0200

    MessageService - delete unneccessary class

--- week7/day3/week7day3Message/src/main/java/com/greenfoxacademy/MessageProceeder.java
+++
@@ -1,9 +0,0 @@
-package com.greenfoxacademy;
-
-public class MessageProceeder {
-
-  public void processMessage(String s, String s1) {
-
-  }
-}
-
--- week7/day3/week7day3Message/src/main/java/com/greenfoxacademy/MessageserviceApplication.java
+++ week7/day3/week7day3Message/src/main/java/com/greenfoxacademy/MessageserviceApplication.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy;
 
+import com.greenfoxacademy.configuration.MessageProceeder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
