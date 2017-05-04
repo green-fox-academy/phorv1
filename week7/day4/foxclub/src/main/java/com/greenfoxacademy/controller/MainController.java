2c3b439856189be78d51e526fd33822befa9bae9
Patrik <horvath.patrik001@gmail.com>
2017-05-04 16:40:59 +0200

    FoxClub - put index.html into templates

--- week7/day4/foxclub/src/main/java/com/greenfoxacademy/controller/MainController.java
+++ week7/day4/foxclub/src/main/java/com/greenfoxacademy/controller/MainController.java
@@ -1,9 +1,17 @@
 package com.greenfoxacademy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
 
+  @RequestMapping(value = "/")
+  public ModelAndView infoPage(){
+    ModelAndView m = new ModelAndView();
+    m.setViewName("index");
+    return m;
+  }
 }
---
+++ week7/day4/foxclub/src/main/java/com/greenfoxacademy/model/Fox.java
@@ -0,0 +1,8 @@
+package com.greenfoxacademy.model;
+
+/**
+ * Created by Lenovo on 2017.05.04..
+ */
+public class Fox {
+
+}
