55102b6c50bc56cef8282e1850b348a05db855aa
Patrik <horvath.patrik001@gmail.com>
2017-05-09 17:16:22 +0200

    todossql - add new task

--- week8/todossql/src/main/java/com/greenfox/phorv1/controller/TodoController.java
+++ week8/todossql/src/main/java/com/greenfox/phorv1/controller/TodoController.java
@@ -1,10 +1,14 @@
 package com.greenfox.phorv1.controller;
 
+import com.greenfox.phorv1.model.Todo;
 import com.greenfox.phorv1.repo.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -14,7 +18,7 @@ public class TodoController {
   @Autowired
   TodoRepository repository;
 
-  @RequestMapping(value = {"/", "list"})
+  @RequestMapping(value = {"/", "/list"})
   public String list(@RequestParam(value = "isActive", required = false) boolean isActive,
           Model model) {
     if (isActive) {
@@ -23,6 +27,18 @@ public class TodoController {
       model.addAttribute("todos", repository.findAll());
     }
     return "/todolist";
+  }
+
+  @GetMapping(value = "/add")
+  public String addTodo(Model model) {
+    model.addAttribute("todos", new Todo());
+    return "addtodo";
+  }
+
+  @PostMapping (value = "/add")
+  public String saveTodo(Todo todo) {
+    repository.save(todo);
+    return "redirect:/todo/";
   }
 
 }
--- week8/todossql/src/main/java/com/greenfox/phorv1/model/Todo.java
+++ week8/todossql/src/main/java/com/greenfox/phorv1/model/Todo.java
@@ -17,8 +17,8 @@ public class Todo {
   long id;
 
   String title;
-  boolean isUrgent;
-  boolean isDone;
+  boolean isUrgent = false;
+  boolean isDone = false;
 
   public Todo() {
   }
