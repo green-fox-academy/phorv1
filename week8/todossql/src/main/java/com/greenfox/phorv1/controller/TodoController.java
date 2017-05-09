e6994536427b989d8066969dba917bce9adca5cc
Patrik <horvath.patrik001@gmail.com>
2017-05-09 17:36:06 +0200

    todossql - remove task

--- week8/todossql/src/main/java/com/greenfox/phorv1/controller/TodoController.java
+++ week8/todossql/src/main/java/com/greenfox/phorv1/controller/TodoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,6 +39,12 @@ public class TodoController {
   @PostMapping (value = "/add")
   public String saveTodo(Todo todo) {
     repository.save(todo);
+    return "redirect:/todo/";
+  }
+
+  @GetMapping(value = "/{id}/delete")
+  public String removeTodo(@PathVariable("id") long id) {
+    repository.delete(id);
     return "redirect:/todo/";
   }
 
